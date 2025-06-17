87929c46fe27288194fc4dcf29e08abf0beeafad
Dhara_shr <dharaarchana2020@gmail.com>
2025-06-17 11:01:57 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,45 @@
+# def sum(m1,m2):
+#     return (m1+m2)
+# m1= int(input("enter a number"))
+# m2= int(input("enter 2nd number"))
+# s= sum(m1,m2)
+# print(s)
+
+# ML= input("any question bout ml?")
+# match ML:
+#     case "what is ml":
+#         print("ml is machine learning")
+#     case "are we making a ml model?":
+#         print("yes")   
+
+# for i in range(4,20):
+#     print(i)
+
+# while True:
+#     print("pc dead")
+
+# import random
+# num= random.randint(2,20)
+# print(num)
+
+# import random
+# snum= random.randint(1,10)
+# print("hint: number lies between 1 to 10")
+# attempts=5
+# while attempts>0:
+#   guss= int(input("guss a number"))
+#   if guss > snum:
+#     print ("nope, secret number is lesser")
+#   elif guss < snum:
+#     print("nope, secret number is greater")
+#   elif guss == snum:
+#     print("yay right choice")
+#     break
+#   attempts= attempts-1
+
+    
+
+
+  
+      
+
